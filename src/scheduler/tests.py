6bac8c5dd623c382e710a7e75550b0ff748dd074
NubeDev <aidan@enviroservices.com.au>
2020-12-30 16:31:34 +1000

    added tests

--- src/scheduler/tests.py
+++ src/scheduler/tests.py
@@ -1,18 +1,17 @@
 
-
 import json
 
 with open('test-payload-from-dashboard.json') as json_file:
     data = json.load(json_file)
-    print(json_file)
-    for p in data['weekly']:
+    print(type(json_file))
+    # for p in data['weekly']:
+    for p in data:
         print(p)
 
 
 def is_json_key_present(json, key):
     try:
-        buf = json[key]
+        json[key]
     except KeyError:
         return False
-
     return True
